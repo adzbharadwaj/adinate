2e8aca2b6ac71717e79d91f8125123a791cf5ea8
adityabharadwaj <adityabharadwaj@192.168.2.3>
2018-01-30 19:22:53 +0530

    Added log4j2 in the framework!!

---
+++ src/test/java/com/ad/adinate/utils/LoggingDemo.java
@@ -0,0 +1,14 @@
+package com.ad.adinate.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingDemo {
+	public static final Logger log4 = LogManager.getLogger(LoggingDemo.class.getName());
+	
+	public static void main(String[] args) {
+		log4.debug("Debug ....");
+		log4.info("Info....");
+		log4.error("Error....");
+	}
+}
