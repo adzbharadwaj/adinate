1bd616e46cde81dabd198eb53546122d8c6b167f
adityabharadwaj <adityabharadwaj@192.168.2.3>
2018-01-30 16:24:02 +0530

    First Commit!

---
+++ src/main/java/com/ad/adinate/utils/App.java
@@ -0,0 +1,13 @@
+package com.ad.adinate.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
